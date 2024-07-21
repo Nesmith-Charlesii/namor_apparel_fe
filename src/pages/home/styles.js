25f5599f3e3d1b89b1940871459a2ab98b4f63d6
nesmith-charlesii <nesmith.charlesii@gmail.com>
2024-07-21 17:11:02 -0400

    Figma design file needed. Layout placed on hold

--- src/pages/home/styles.js
+++ src/pages/home/styles.js
@@ -4,10 +4,13 @@ import theme from "../../shared/styles/theme";
 const useStyles = createUseStyles({
     container: {
         height: "100vh",
-        width: "100vw",
         backgroundColor: theme.colors.primary,
     },
     title: {
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "100%",
         '& h1': {
             fontFamily: theme.fonts.primary,
             letterSpacing: 2,
