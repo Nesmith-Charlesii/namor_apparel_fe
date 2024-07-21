e5f08f683226a0f68bbd4d927e6a77a835ad8214
nesmith-charlesii <nesmith.charlesii@gmail.com>
2024-07-21 03:11:23 -0400

    Sphere react component render to home page

--- src/pages/home/components/threeJs/sphere.js
+++ src/pages/home/components/threeJs/sphere.js
@@ -15,13 +15,22 @@ const Sphere = () => {
 
     // Create a sphere geometry and material, then combine them into a mesh
     const geometry = new THREE.SphereGeometry(3, 64, 64);
-    const material = new THREE.MeshStandardMaterial({ color: '#00ff83' });
+    const material = new THREE.MeshStandardMaterial({ 
+        color: '#00ff83' 
+    });
     const mesh = new THREE.Mesh(geometry, material);
     scene.add(mesh);
+    
+    // Light
+    const light = new THREE.PointLight(0xffffff, 70, 100, 1.7)
+    light.position.set(0, 10, 10)
+    scene.add(light)
 
     // Create a camera and set its position
-    const camera = new THREE.PerspectiveCamera(45, 800, 600);
-    //camera.position.z = 10; // Adjust as needed
+    const camera = new THREE.PerspectiveCamera(45, 800/600);
+    camera.position.z = 10;
+    scene.add(mesh)
+
 
     // Create a renderer and attach it to the canvas
     const renderer = new THREE.WebGLRenderer({ canvas });
@@ -34,7 +43,9 @@ const Sphere = () => {
     };
   }, [classes]);
 
-  return <canvas ref={canvasRef} className={classes.webGl} />;
+  return (
+    <canvas ref={canvasRef} className={classes.webGl} />
+  )
 };
 
 export default Sphere;
