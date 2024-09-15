ae3067fd051f787ba8db329aef345d9f9618772e
nesmith-charlesii <nesmith.charlesii@gmail.com>
2024-09-14 21:37:19 -0400

    keyframes for dot push

--- src/pages/home/pageStyles.js
+++ src/pages/home/pageStyles.js
@@ -14,8 +14,15 @@ const useStyles = createUseStyles({
         textAlign: "center",
         padding: "0px 20px 0px 20px"
     },
-    '@keyframes bounce': {
-        
+    '@keyframes leftDotPush': {
+        '0%': { left: 0 },
+        '50%': { left: 100 },
+        '100%': { left: 0 }
+    },
+    '@keyframes rightDotPush': {
+        '0%': { right: 0 },
+        '50%': { right: 100 },
+        '100%': { right: 0 }
     },
     titleAcronym: {
         fontSize: "12rem",
@@ -27,20 +34,21 @@ const useStyles = createUseStyles({
         },
         '& span:nth-child(2)': {
             position: 'relative',
-            
+            animation: '$leftDotPush 1s ease-in'
         },
         '& span:nth-child(3)': {
             opacity: 0
         },
         '& span:nth-child(4)': {
             position: 'relative',
-            
+            animation: '$rightDotPush 1s ease-in'
         },
         '& span:nth-child(5)': {
             opacity: 0
         }
     },
     titleName: {
+        opacity: 0,
         fontSize: "4rem",
         letterSpacing: 24,
         marginTop: 0,
