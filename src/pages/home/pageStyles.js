192e205bcc693fb13dfc0ee16c246218d51f5dab
nesmith-charlesii <nesmith.charlesii@gmail.com>
2024-09-14 22:58:21 -0400

    blur effect to H.O.N

--- src/pages/home/pageStyles.js
+++ src/pages/home/pageStyles.js
@@ -14,15 +14,20 @@ const useStyles = createUseStyles({
         textAlign: "center",
         padding: "0px 20px 0px 20px"
     },
-    '@keyframes leftDotPush': {
-        '0%': { left: 0 },
-        '50%': { left: 100 },
-        '100%': { left: 0 }
+    '@keyframes blur': {
+        "0%": {
+            opacity: 0,
+            filter: "blur(5px)",
+            right: 100
+        },
+        "100%": {
+            opacity: 1,
+            filter: "blur(0px)",
+            right: 0
+        }
     },
-    '@keyframes rightDotPush': {
-        '0%': { right: 0 },
-        '50%': { right: 100 },
-        '100%': { right: 0 }
+    '@keyframes popUp': {
+        '0%': {  }
     },
     titleAcronym: {
         fontSize: "12rem",
@@ -34,14 +39,14 @@ const useStyles = createUseStyles({
         },
         '& span:nth-child(2)': {
             position: 'relative',
-            animation: '$leftDotPush 1s ease-in'
+            animation: "$blur 6s ease-out"
         },
         '& span:nth-child(3)': {
             opacity: 0
         },
         '& span:nth-child(4)': {
             position: 'relative',
-            animation: '$rightDotPush 1s ease-in'
+            animation: "$blur 4s ease-out"
         },
         '& span:nth-child(5)': {
             opacity: 0
