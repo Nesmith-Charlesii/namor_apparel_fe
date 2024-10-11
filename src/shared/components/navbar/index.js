1701a0a579736b7cc64211d4e69712859b839ca6
nesmith-charlesii <nesmith.charlesii@gmail.com>
2024-10-11 14:43:56 -0400

    nav burger menu created

--- src/shared/components/navbar/index.js
+++ src/shared/components/navbar/index.js
@@ -1,37 +1,46 @@
-import React, { useRef, useState } from 'react';
+import React /*{ useRef, useState }*/ from 'react';
 import useStyles from './pageStyles';
-import gsap from 'gsap';
-import { useGSAP } from '@gsap/react';
+// import gsap from 'gsap';
+// import { useGSAP } from '@gsap/react';
 
 const Navbar = () => {
     const classes = useStyles();
-    const [activeMenu, setActiveMenu] = useState(true);
+    // const [activeMenu, setActiveMenu] = useState(true);
 
-    const navbar = useRef()
-    const navbarLinks = useRef()
+    // const navbar = useRef()
+    // const navbarLinks = useRef()
 
-    useGSAP(() => {
-        const tl = gsap.timeline()
+    // useGSAP(() => {
+    //     const tl = gsap.timeline()
 
-        tl.fromTo(
-            navbarLinks.current.querySelectorAll("div:nth-child(1),div:nth-child(2),div:nth-child(3),div:nth-child(4), div:nth-child(5)"),
-            {
-                y: -50,
-                opacity: 0,
-            },
-            {
-                y: 0,
-                stagger: { each: 0.2, from: "start" },
-                opacity: .5,
-                duration: .5,
-                delay: 3.5
-            }
-        )
-    })
+    //     tl.fromTo(
+    //         navbarLinks.current.querySelectorAll("div:nth-child(1),div:nth-child(2),div:nth-child(3),div:nth-child(4), div:nth-child(5)"),
+    //         {
+    //             y: -50,
+    //             opacity: 0,
+    //         },
+    //         {
+    //             y: 0,
+    //             stagger: { each: 0.2, from: "start" },
+    //             opacity: .5,
+    //             duration: .5,
+    //             delay: 3.5
+    //         }
+    //     )
+    // })
 
     return (
-        <div className={classes.navMenu}>
-
+        <div className={classes.navBar}>
+            <div className={classes.navContent}>
+                <div className={classes.navLogo}>
+                    logo placement
+                </div>
+                <div className={classes.navIcon}>
+                    <div className={classes.navLine}></div>
+                    <div className={classes.navLine}></div>
+                    <div className={classes.navLine}></div>
+                </div>
+            </div>
         </div>
         // <>
         //     {activeMenu &&
--- src/shared/components/navbar/pageStyles.js
+++ src/shared/components/navbar/pageStyles.js
@@ -2,29 +2,59 @@ import { createUseStyles } from "react-jss";
 //import theme from "../../styles/theme";
 
 const useStyles = createUseStyles({
-    navbar: {
-        height: "100px",
+    navBar: {
         position: "fixed",
+        height: "80px",
+        width: "100%",
         display: "flex",
-        alignItems: "center",
+        alignItems: 'center',
+        backgroundColor: "transparent",
         zIndex: 2,
-        width: "100%",
-        backgroundColor: "transparent"
     },
-    navLinks: {
-        //color: "rgb(60, 60, 60)",
-        width: "100%",
-        color: "rgb(240, 240, 240)",
-        fontFamily: "Courier New",
+    navContent: {
         display: "flex",
         justifyContent: "space-between",
         alignItems: "center",
-        listStyleType: "none",
-        fontSize: "1rem",
-        fontWeight: 600
-,       padding: "0px 80px",
-        letterSpacing: "2px"
-    }
+        width: "100%",
+        padding: "0px 80px"
+    },
+    navIcon: {
+        height: "50px",
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "space-evenly"
+    },
+    navLogo: {
+        color: "rgb(240, 240, 240)"
+    },
+    navLine: {
+        height: ".5px",
+        width: "50px",
+        backgroundColor: "rgb(240, 240, 240)"
+    },
+//     navbar: {
+//         height: "100px",
+//         position: "fixed",
+//         display: "flex",
+//         alignItems: "center",
+//         zIndex: 2,
+//         width: "100%",
+//         backgroundColor: "transparent"
+//     },
+//     navLinks: {
+//         //color: "rgb(60, 60, 60)",
+//         width: "100%",
+//         color: "rgb(240, 240, 240)",
+//         fontFamily: "Courier New",
+//         display: "flex",
+//         justifyContent: "space-between",
+//         alignItems: "center",
+//         listStyleType: "none",
+//         fontSize: "1rem",
+//         fontWeight: 600
+// ,       padding: "0px 80px",
+//         letterSpacing: "2px"
+//     }
 })
 
 export default useStyles
