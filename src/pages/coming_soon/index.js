c8195da1256518471c7c91af027f316ff63f0efc
nesmith-charlesii <nesmith.charlesii@gmail.com>
2024-11-17 14:25:52 -0500

    logo animation placement complete. adding digital clock effect

--- src/pages/coming_soon/index.js
+++ src/pages/coming_soon/index.js
@@ -17,32 +17,46 @@ const ComingSoon = () => {
             autoAlpha: 1
         })
 
-        tl.to('svg', { 
+        tl.to('svg', {
             scale: 1.2,
             duration: 4
-         })
+        })
+
+        tl.from('#honLogo .acro',
+            {
+                opacity: 0,
+                yPercent: 215,
+                // skewY: 60,
+                // skewX: 30,
+                scaleY: .5,
+                stagger: { each: .4, from: "end" }
+            }, 0
+        )
+
+        tl.from('#honLogo .period',
+            {
+                filter: "blur(10px)",
+                scale: 0,
+                stagger: { each: .4, from: "start" }
+            }, 0
+        )
 
         tl.from('#honLogo .st1',
-            { 
+            {
                 opacity: 0,
                 yPercent: -200, 
-                stagger: { each: 0.1, from: "random" }
+                stagger: { each: 0.09, from: "end" }
             }, 0
         )
 
-        tl.from('#honLogo .period', 
-            {
-                scale: 0,
-                stagger: { each: 0.5, from: "end" }
-            }, 0
-        )
+
 
     }, { dependencies: [], scope: containerRef })
 
     return (
         <div className={classes.container} ref={containerRef}>
             <div>
-                <Logo ref={logoRef}/>
+                <Logo ref={logoRef} />
             </div>
         </div>
     )
