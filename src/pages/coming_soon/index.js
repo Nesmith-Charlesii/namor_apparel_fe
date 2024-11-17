dd231c9d600ba35ad1dc252731b06db360e9e95a
nesmith-charlesii <nesmith.charlesii@gmail.com>
2024-11-16 22:24:08 -0500

    exporting logo from figma as individual SVGs

--- src/pages/coming_soon/index.js
+++ src/pages/coming_soon/index.js
@@ -2,24 +2,33 @@ import React, { useRef } from 'react';
 import useStyles from './styles';
 import gsap from 'gsap';
 import { useGSAP } from '@gsap/react';
-import { ReactComponent as Logo } from '../../assets/image/H.o.N HOUSE OF NAMOR.svg';
+import { ReactComponent as Logo } from '../../assets/image/Vector.svg';
 
 const ComingSoon = () => {
     const classes = useStyles()
 
     const containerRef = useRef()
+    const logoRef = useRef()
 
     useGSAP(() => {
-        const tl = gsap.timeline({defaults: { ease: 'power4.out', duration: .7 }})
+        const tl = gsap.timeline()
 
-
+        tl.from('#hon .st1',
+            { 
+                opacity: 0,
+                xPercent: -200, 
+                rotatex: "270deg", 
+                stagger: { each: 0.2 }, 
+                duration: .7 
+            }
+        )
 
     }, { dependencies: [], scope: containerRef })
 
     return (
         <div className={classes.container} ref={containerRef}>
             <div>
-                <Logo />
+                <Logo ref={logoRef}/>
             </div>
         </div>
     )
