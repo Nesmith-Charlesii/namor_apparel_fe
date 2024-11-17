b716372f555ef5be4ecb915628755cad315d4e44
nesmith-charlesii <nesmith.charlesii@gmail.com>
2024-11-16 22:36:20 -0500

    svg vectors given classes. updated coming-soon component

--- src/pages/coming_soon/index.js
+++ src/pages/coming_soon/index.js
@@ -2,7 +2,7 @@ import React, { useRef } from 'react';
 import useStyles from './styles';
 import gsap from 'gsap';
 import { useGSAP } from '@gsap/react';
-import { ReactComponent as Logo } from '../../assets/image/Vector.svg';
+import { ReactComponent as Logo } from '../../assets/image/pathVector.svg';
 
 const ComingSoon = () => {
     const classes = useStyles()
@@ -13,14 +13,23 @@ const ComingSoon = () => {
     useGSAP(() => {
         const tl = gsap.timeline()
 
-        tl.from('#hon .st1',
+        tl.set('g', {
+            autoAlpha: 1
+        })
+
+        tl.to('svg', { 
+            scale: 1.2,
+            duration: 4
+         })
+
+        tl.from('#honLogo .st1',
             { 
                 opacity: 0,
-                xPercent: -200, 
+                yPercent: -200, 
                 rotatex: "270deg", 
-                stagger: { each: 0.2 }, 
+                stagger: { each: 0.1, from: "start" }, 
                 duration: .7 
-            }
+            }, 0
         )
 
     }, { dependencies: [], scope: containerRef })
