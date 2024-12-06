8d671ae19bcd300e78b40385cd491111adbbc16b
nesmith-charlesii <nesmith.charlesii@gmail.com>
2024-12-05 20:29:00 -0500

    removed svgs and instead opted for strings

--- src/pages/coming_soon/index.js
+++ src/pages/coming_soon/index.js
@@ -7,87 +7,97 @@ import { ReactComponent as Logo } from '../../assets/image/pathVector.svg';
 const ComingSoon = () => {
     const classes = useStyles()
 
-    const containerRef = useRef()
-    const logoRef = useRef()
-    const comingSoonRef = useRef()
+    const titleText = "HOUSE of NAMOR";
+    const letterArr = titleText.split("")
 
-    useGSAP(() => {
-        const tl = gsap.timeline()
+    // const containerRef = useRef()
+    const titleContainer = useRef();
+    const titleAcronym = useRef();
+    // const comingSoonRef = useRef()
 
-        tl.set('g', {
-            autoAlpha: 1
-        })
+    // useGSAP(() => {
+    //     const tl = gsap.timeline()
 
-        tl.to('svg', {
-            scale: 1.2,
-            duration: 3,
-            ease: "sine.out"
-        })
+    //     tl.set('g', {
+    //         autoAlpha: 1
+    //     })
 
-        tl.from('#honLogo .acro',
-            {
-                filter: "blur(10px)",
-                opacity: 0,
-                yPercent: 215,
-                scaleY: .5,
-                skewY: 60,
-                skewX: 30,
-                stagger: { each: .4, from: "end" },
-            }, 0
-        )
+    //     tl.to('svg', {
+    //         scale: 1.2,
+    //         duration: 6,
+    //         ease: "power1.out",
+    //         delay: 2
+    //     })
 
-        tl.from('#honLogo .period',
-            {
-                filter: "blur(10px)",
-                scale: 0,
-                stagger: { each: .3, from: "start" },
-            }, 0
-        )
+    //     tl.from('#honLogo .acro',
+    //         {
+    //             filter: "blur(10px)",
+    //             opacity: 0,
+    //             yPercent: 215,
+    //             scaleY: .5,
+    //             skewY: 60,
+    //             skewX: 30,
+    //             stagger: { each: .5, from: "end" },
+    //             delay: 2
+    //         }, 0
+    //     )
 
-        tl.from('#honLogo .st1',
-            {
-                filter: "blur(10px)",
-                opacity: 0,
-                yPercent: -200, 
-                stagger: { each: 0.09, from: "end" },
-            }, 0
-        )
+    //     tl.from('#honLogo .period',
+    //         {
+    //             filter: "blur(10px)",
+    //             scale: 0,
+    //             stagger: { each: .3, from: "start" },
+    //             delay: 2
+    //         }, 0
+    //     )
 
-        tl.fromTo(comingSoonRef.current,
-            {
-                opacity: 0,
-            },
-            {
-                opacity: .5,
-                duration: 2,
-                repeat: -1,
-                repeatDelay: 1,
-                yoyo: true,
-                yoyoEase: "sine.out",
-                ease: "sine.out"
-            }, 
-        )
+    //     tl.from('#honLogo .st1',
+    //         {
+    //             filter: "blur(10px)",
+    //             opacity: 0,
+    //             yPercent: -200, 
+    //             stagger: { each: 0.1, from: "end" },
+    //             delay: 2
+    //         }, 0
+    //     )
 
-        tl.to('#honLogo path',
-            {
-                opacity: 0.7,
-                filter: "blur(2px)",
-                stagger: { each: .09, from: "random" },
-                repeat: -1,
-                repeatDelay: 1,
-                yoyo: true,
-                ease: "sine.out"
-            }, 
-        )
+    //     tl.to(
+    //         comingSoonRef.current,
+    //         {
+    //             opacity: 1,
+    //             duration: 2,
+    //             ease: "sine.in",
+    //         }, "-=1"
+    //     );
 
-    }, { dependencies: [], scope: containerRef })
+    //     tl.to('#honLogo path',
+    //         {
+    //             opacity: 0.7,
+    //             filter: "blur(2px)",
+    //             stagger: { each: .07, from: "random" },
+    //             repeat: -1,
+    //             repeatDelay: 1,
+    //             yoyo: true,
+    //             ease: "sine.out"
+    //         }, "-=2"
+    //     )
+
+    // }, { dependencies: [], scope: containerRef })
 
     return (
-        <div className={classes.container} ref={containerRef}>
-            <Logo className={classes.logo} ref={logoRef} />
-            <div className={classes.comingSoon} ref={comingSoonRef}>
-                <p>COMING</p>
-                <p>SOON</p>         
+        <div className={classes.container} /*ref={containerRef}*/>
+            <div className={classes.pageContent} /*ref={comingSoonRef}*/>
+                <h2 className={classes.comingSoon}>COMING</h2>
+                <div className={classes.titleContainer} ref={titleContainer}>
+                    <div className={classes.titleAcronym} ref={titleAcronym}>
+                        <div>H</div>
+                        <div>.</div>
+                        <div>o</div>
+                        <div>.</div>
+                        <div>N</div>
+                    </div>
+                </div>
+                <h2 className={classes.comingSoon}>SOON</h2>
             </div>
         </div>
     )
--- src/pages/coming_soon/styles.js
+++ src/pages/coming_soon/styles.js
@@ -3,42 +3,59 @@ import theme from "../../shared/styles/theme";
 
 const useStyles = createUseStyles({
     container: {
-        position: "relative",
         height: "100vh",
-        width: "100vw",
-        backgroundColor: "white",
-        display: "grid",
-        placeItems: "center",
-        overflow: "hidden"
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        overflow: 'hidden',
+        margin: 0
     },
-    logo: {
-        position: 'relative',
-        zIndex: 2,
-        bottom: "45px",
-        maxWidth: "100%",
-        maxHeight: "100%",
-        width: "auto",
-        height: "auto"
+    pageContent: {
+        // opacity: 1,
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
     },
     comingSoon: {
         color: "#E0D2BB",
-        position: 'absolute',
+        fontSize: "100px",
+        fontFamily: theme.fonts.primary,
+        fontWeight: 600
+    },
+    titleContainer: {
+        zIndex: 1,
         display: "flex",
         flexDirection: "column",
+        justifyContent: "center",
         alignItems: "center",
-        width: "100%",
-        zIndex: 1,
-        fontSize: "200px",
+        marginLeft: "41px",
         fontFamily: theme.fonts.primary,
-        fontWeight: 600,
-        letterSpacing: 20,
-        '& p:nth-child(1)': {
-            marginBottom: "0px"
+        //color: "rgb(60, 60, 60)",
+        color: "rgb(85, 85, 85)"
+    },
+    titleAcronym: {
+        display: "flex",
+        fontSize: "9rem",
+        letterSpacing: 44,
+        marginTop: 0,
+        marginBottom: 0,
+        '& div:nth-child(1)': {
+            opacity: 1
         },
-        '& p:nth-child(2)': {
-            marginTop: "250px",
+        '& div:nth-child(2)': {
+            opacity: 1
+        },
+        '& div:nth-child(3)': {
+            opacity: 1
+        },
+        '& div:nth-child(4)': {
+            opacity: 1
+        },
+        '& div:nth-child(5)': {
+            opacity: 1
         }
-    }
+    },
 })
 
 export default useStyles;
